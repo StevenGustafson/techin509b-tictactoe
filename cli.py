c0df326f3e16992adc7de1c6d1a23823edef9f3a
ShunxiWu <shunxiwu@gmail.com>
2023-10-31 01:15:11 -0700

    HM

--- cli.py
+++ cli.py
@@ -2,18 +2,44 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
+from logic import make_empty_board, get_winner, print_board, is_board_full, other_player
 
 
 # Reminder to check all the tests
 
+
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
-    while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        # TODO: Input a move from the player.
-        # TODO: Update the board.
-        # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
+    current_player = "X"
+    while winner is None:
+        print_board(board)
+        print(f"It's {current_player}'s turn.")
+
+        # Input a move from the player
+        try:
+            row = int(input("Enter the row (1, 2, or 3): ")) - 1  # Adjust to 0-based index
+            col = int(input("Enter the column (1, 2, or 3): ")) - 1  # Adjust to 0-based index
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        # Check if the move is valid
+        if row not in (0, 1, 2) or col not in (0, 1, 2) or board[row][col] != '':
+            print("Invalid move. Try again.")
+            continue
+
+        # Update the board
+        board[row][col] = current_player
+
+        # Check if someone has won
+        winner = get_winner(board)
+        if winner:
+            print_board(board)
+            if winner == "Tie":
+                print("It's a tie! The game is a draw.")
+            else:
+                print(f"Player {winner} wins!")
+        else:
+            # Switch to the other player's turn
+            current_player = 'O' if current_player == 'X' else 'X'
--- logic.py
+++ logic.py
@@ -2,21 +2,69 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-
 def make_empty_board():
     return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
     ]
-
 
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None  # FIXME
+    for player in ['X', 'O']:
+        # Check rows for a win
+        for i in range(3):
+            row_win = True
+            for j in range(3):
+                if board[i][j] != player:
+                    row_win = False
+                    break
+            if row_win:
+                return player
+
+        # Check columns for a win
+        for i in range(3):
+            col_win = True
+            for j in range(3):
+                if board[j][i] != player:
+                    col_win = False
+                    break
+            if col_win:
+                return player
+
+        # Check diagonal (top-left to bottom-right) for a win
+        diagonal_win = True
+        for i in range(3):
+            if board[i][i] != player:
+                diagonal_win = False
+                break
+        if diagonal_win:
+            return player
+
+        # Check diagonal (top-right to bottom-left) for a win
+        diagonal_win = True
+        for i in range(3):
+            if board[i][2 - i] != player:
+                diagonal_win = False
+                break
+        if diagonal_win:
+            return player
+
+    return None  # No winner yet
 
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return 'X' if player == 'O' else 'O'
+
+def print_board(board):
+    for row in board:
+        print(row)
+
+def is_board_full(board):
+    for row in board:
+        for cell in row:
+            if cell == ' ':
+                return False  # 如果有一个空格，游戏板未满
+    return True  # 游戏板已满
